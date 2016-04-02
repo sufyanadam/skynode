ea4a5c47b38e51138ada5fd9ebfd1018b43c16b3
Sufyan Adam <pair+sufyanadam@gmail.com>
2016-04-01 21:53:40 -0700

    Exchange oauth code for access token
    Use the npm `request` module to make a final `POST`
    request to the nest oauth2 endpoint to exchange the
    access code for an access token. The `simple-oauth2`
    module didn't seem to work, so used the request
    module instead.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,48 @@
 var express = require('express');
 var router = express.Router();
+var request  = require('request');
+var oauth2  = require('simple-oauth2')({
+  clientID: '082ece14-4bba-48bc-8020-bcd00611d3f1',
+  clientSecret: 'cCkG0Op1UPEs5iPhCxbLxOwbf',
+  site: 'https://home.nest.com/login',
+  authorizationPath: '/oauth2',
+  tokenPath: 'oauth2/access_token'
+});
+
+var authorization_uri = oauth2.authCode.authorizeURL({ state: 'emacs-is-awesome' });
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Skynode' });
+});
+
+router.get('/nest_oauth', function(req, res, next) {
+  res.redirect(authorization_uri);
+});
+
+router.get('/oauth_callback', function(req, res, next){
+  var code = req.query.code;
+
+  request.post(
+    'https://api.home.nest.com/oauth2/access_token',
+    {
+      form: {
+        client_id: '082ece14-4bba-48bc-8020-bcd00611d3f1',
+        code: req.query.code,
+        client_secret: 'cCkG0Op1UPEs5iPhCxbLxOwbf',
+        grant_type: 'authorization_code'
+      }
+    }, handleAccessTokenResponse
+  );
+
+  function handleAccessTokenResponse(error, response, body) {
+    if (error) {
+      res.render('index', { error: error })
+      return;
+    }
+
+    var token = JSON.parse(body).access_token;
+    res.render('index', { access_token: token });
+  }
 });
 
 module.exports = router;
